670682fc9def6a81a9f82bf46c1adf6953349f64
mahdi abdulkareem <mahdiimad64@gmail.com>
2020-08-21 14:46:29 -0700

    finish

--- main.js
+++ main.js
@@ -1,59 +1,46 @@
-var fs = require('fs');
+var fs = require("fs");
 
+function readPlain() {
+  const file = fs.readFileSync("input.csv", "utf8", (err) => {
+    if (err) throw err;
+  });
+  const lineArr = file.split("\n");
+  const header = lineArr[0].split(",");
+  lineArr.splice(0, 1);
 
-function readPlain (){
-
-const file  = fs.readFileSync("input.csv" ,"utf8" , (err) => {
-    if(err) throw err;
-  
-});
-const arrLines = file.split('\n');
-const Header = arrLines[0].split(',');
-arrLines.splice(0, 1);
-const result = [];
-arrLines.map((item1 , index1=2) => {       
-    const x = item1.split(',');  
-    var obj = {};
-    x.map((item2 , index2)=>{        
-        const m = Header[index2];
-         obj[m] = item2 ;  
-    })
-    
-    result.push(obj);
-})
-return result;
-
-};
+  return lineArr.map((line, lineIndex = 2) => {
+    const dataArr = line.split(",");
+    return dataArr.reduce((obj, data, dataIndex) => {
+      const key = header[dataIndex];
+      obj[key] = data;
+      return obj;
+    }, {});
+  });
+}
 
 //Read File
-// console.log(readPlain());
+console.log(readPlain());
 
-
-function saveToFile (arr){
-   
-        fs.writeFile('jsonfile.json',JSON.stringify(arr) ,  (err) => {
-            if (err) throw err;
-            console.log('Saved!');
-          });
-};
+function saveToFile(arr) {
+  fs.writeFile("jsonfile.json", JSON.stringify(arr), (err) => {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+}
 
 //save jsonFile
-// saveToFile(readPlain());
+saveToFile(readPlain());
 
-
-function readJsonFile (){
-  let file =   fs.readFileSync("jsonfile.json" , (err) => {
-      if(err) {
-        //   throw err;
-          console.log('Error : '+err);
-        }
-
+function readJsonFile() {
+  let file = fs.readFileSync("jsonfile.json", (err) => {
+    if (err) {
+      //   throw err;
+      console.log("Error : " + err);
+    }
   });
-    let parseFile = JSON.parse(file);
-    return parseFile; 
+  let parseFile = JSON.parse(file);
+  return parseFile;
 }
 
 //read json file
-console.log(readJsonFile());
-
-
+// console.log(readJsonFile());
